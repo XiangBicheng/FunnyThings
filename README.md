3af63755290259a25806e7fc31983bbc5f3d00bd
Xiang Bicheng <xiangbcyx@qq.com>
2018-05-28 18:55:40 +0800

    subtitle conversion

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 ## List:
 
 - batch rename: `python .\batch_rename.py 'replace_string'`
+- subtitle conversion: `python .\subtitle_conversion 'file_path'`
 - ...
+
--- subtitle_conversion.py
+++ subtitle_conversion.py
@@ -1,13 +1,18 @@
+'''
+This is a script for converting subtitles with a timeline to no-timeline.
+'''
+
 import re, sys,os
 
 file_path = os.path.abspath(sys.argv[1])
 
 f = open(file_path, encoding='utf-8')
-subtitle = f.read().replace('\n', '')
+subtitle = f.read().replace('\n', ' ')
 f.close()
 p = re.compile(r'\d+:\d{2}:\d{2},\d+\s-->\s\d{2}:\d{2}:\d{2},\d+')
 newsubtitle = p.sub(' ', subtitle)
-
+p2 = re.compile(r'\d+\s+')
+newsubtitle = p2.sub('', newsubtitle)
 file_dest = file_path.replace('.srt', '.txt')
 with open(file_dest, 'w', encoding='utf-8') as f:
     f.write(newsubtitle)
