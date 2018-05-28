f49b6033de1b03d5f6995f217e88f1bf915abe20
Xiang Bicheng <xiangbcyx@qq.com>
2018-05-28 18:31:25 +0800

    add subtitle conversion scripts

---
+++ subtitle_conversion.py
@@ -0,0 +1,13 @@
+import re, sys,os
+
+file_path = os.path.abspath(sys.argv[1])
+
+f = open(file_path, encoding='utf-8')
+subtitle = f.read().replace('\n', '')
+f.close()
+p = re.compile(r'\d+:\d{2}:\d{2},\d+\s-->\s\d{2}:\d{2}:\d{2},\d+')
+newsubtitle = p.sub(' ', subtitle)
+
+file_dest = file_path.replace('.srt', '.txt')
+with open(file_dest, 'w', encoding='utf-8') as f:
+    f.write(newsubtitle)
